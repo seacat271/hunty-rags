23d9a604332cd4d676d2c6595572c62be4fa281b
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-03 16:23:15 +0300

    add filter input

---
+++ src/components/FilterHeader/FilterHeader.jsx
@@ -0,0 +1,11 @@
+import { FaSistrix } from "react-icons/fa6";
+export const FilterHeader = () => {
+  return (
+    <>
+      <label>
+        <input />
+        <FaSistrix />
+      </label>
+    </>
+  );
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { Logo } from "../Logo/Logo";
 import { SocialIcons } from "../SocialIcons/SocialIcons";
+import { FilterHeader } from "../FilterHeader/FilterHeader";
 
 export const Header = () => {
   return (
     <header>
       <Logo />
       <SocialIcons />
+      <FilterHeader />
     </header>
   );
 };
