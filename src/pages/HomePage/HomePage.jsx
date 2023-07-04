393ba38dd59e1b9ca6c8dfd878ca4380fff26478
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-04 18:16:38 +0300

    add img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,12 @@
+import { Box } from "../../components/Box/Box";
+import New_collection from "../../utility/new_collection_3.jpg";
+import New_collection2 from "../../utility/new_collection_2.jpg";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box display="flex">
+      <img src={New_collection} alt="new collection" width="50%" />
+      <img src={New_collection2} alt="new collection" width="50%" />
+    </Box>
+  );
 };
