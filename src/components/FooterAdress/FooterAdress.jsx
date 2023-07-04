5bd626f0ef959fc923c4cbc87aae83d79188254c
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-04 17:51:15 +0300

    add icon

--- src/components/FooterAdress/FooterAdress.jsx
+++ src/components/FooterAdress/FooterAdress.jsx
@@ -1,3 +1,4 @@
+import { FaLocationDot, FaPhoneVolume, FaEnvelope } from "react-icons/fa6";
 import { Box } from "../Box/Box";
 import { FooterTitle } from "../FooterTitile/FooterTitle";
 export const FooterAddress = () => {
@@ -8,14 +9,21 @@ export const FooterAddress = () => {
         <ul>
           <li>
             <a href="https://goo.gl/maps/aA86Dh76K2qKcj5f8">
+              <FaLocationDot />
               04210, Kyiv city, Ivasyuka avenu
             </a>
           </li>
           <li>
-            <a href="tel:+38(097)536-98-78">+38(097)536-98-78</a>
+            <a href="tel:+38(097)536-98-78">
+              <FaPhoneVolume />
+              +38(097)536-98-78
+            </a>
           </li>
           <li>
-            <a href="mailto:hunty_rugs@mail.net">hunty_rugs@mail.net</a>
+            <a href="mailto:hunty_rugs@mail.net">
+              <FaEnvelope />
+              hunty_rugs@mail.net
+            </a>
           </li>
         </ul>
       </address>
