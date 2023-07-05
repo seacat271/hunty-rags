21732d0cbd8440f706412d24ca65499f264ae4dc
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-05 11:59:59 +0300

    add map

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -6,14 +6,26 @@ import {
 } from "react-icons/fa6";
 import { StyledBox } from "./SocialIcons.styled";
 export const SocialIcons = () => {
-  const icons = [FaSquareFacebook, FaSquareInstagram, FaTelegram];
-
+  function MakeIconLink(...args) {
+    const iconLink = [];
+    [...args].map((item) => {
+      item[0].href = item[1];
+      iconLink.push(item[0]);
+    });
+    return iconLink;
+  }
+  const icons = MakeIconLink(
+    [FaSquareFacebook, "http://www.facebook.com"],
+    [FaSquareInstagram, "http://www.instagram.com"],
+    [FaTelegram, "https://web.telegram.org/"]
+  );
+  console.log(icons);
   return (
     <StyledBox as="ul" display="flex">
       {icons.map((Icon) => {
         return (
-          <li>
-            <a>
+          <li key={Icon.href}>
+            <a href={Icon.href}>
               <Icon size={36} />
             </a>
           </li>
--- src/components/SocialIcons/SocialIcons.styled.js
+++ src/components/SocialIcons/SocialIcons.styled.js
@@ -3,7 +3,10 @@ import { styled } from "styled-components";
 import { Box } from "../Box/Box";
 
 export const StyledBox = styled(Box)`
-  &svg:hover {
+  a {
+    transition: color 1300 linear 0;
+  }
+  a:hover {
     color: white;
   }
 `;
