02ef0d6956bfaec7bc7adb1b743ffa5aea1b5d5b
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-16 15:10:33 +0300

    add useFormik

--- src/components/Form/RegisterForm.jsx
+++ src/components/Form/RegisterForm.jsx
@@ -1,31 +1,60 @@
-import { Formik, Field, Form } from "formik";
+import { useFormik } from "formik";
 
 export const RegisterForm = () => {
   const initialValues = {
     firstName: "",
     lastName: "",
     email: "",
+    phone: "",
   };
 
-  const handleSubmit = () => {};
+  function handleSubmit(values) {
+    console.log(values);
+  }
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: handleSubmit,
+  });
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form>
-        <label htmlFor="firstName">First Name</label>
-        <Field id="firstName" name="firstName" placeholder="Name" />
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        placeholder="Name"
+        value={formik.values.name}
+        onChange={formik.handleChange}
+      />
 
-        <label htmlFor="lastName">Last Name</label>
-        <Field id="lastName" name="lastName" placeholder="Last name" />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        placeholder="Last name"
+        value={formik.values.lastName}
+        onChange={formik.handleChange}
+      />
 
-        <label htmlFor="email">Email</label>
-        <Field
-          id="email"
-          name="email"
-          placeholder="email@mail.com"
-          type="email"
-        />
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        name="email"
+        placeholder="email@mail.com"
+        type="email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+      />
+      <label htmlFor="phone">Phone</label>
+      <input
+        id="phone"
+        name="phone"
+        placeholder="+38(XXX) XXX-XX-XX"
+        type="tel"
+        value={formik.values.phone}
+        onChange={formik.handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,10 @@
+import { Box } from "../../components/Box/Box";
 import { RegisterForm } from "../../components/Form/RegisterForm";
 
 export const AuthPage = () => {
   return (
-    <div>
+    <Box display="block">
       <RegisterForm />
-    </div>
+    </Box>
   );
 };
