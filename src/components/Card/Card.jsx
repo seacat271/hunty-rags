ace83f9c46f47605e5fa659d5ad511e8f5147e64
seacat271 <105187184+seacat271@users.noreply.github.com>
2023-07-16 18:51:38 +0300

    add

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,18 @@
 import { Box } from "../Box/Box";
+import { Image } from "./Card.styled";
 
 export const Card = ({ item }) => {
   const { image, title, price } = item;
   return (
     <Box padding="10px">
-      <img src={image} alt={title} width="320" height="240" />
-      <Box as="h5" textAlign="center">
+      <Image src={image} alt={title} width="320" height="240" />
+      <Box as="h5" textAlign="center" mb="10px">
         {title}
       </Box>
-      <p>{price}</p>
-      <Box as="button" type="button" textAlign="center">
+      <Box as="p" textAlign="center">
+        PRICE: {price}
+      </Box>
+      <Box as="button" type="button" mb="10px">
         Add to cart
       </Box>
       <Box>
